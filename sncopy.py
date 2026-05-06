cb4079716619611c32670705f58fa3b976694a20
NKL <148013993+NKLNN@users.noreply.github.com>
2026-05-06 04:11:36 -0500

    Add files via upload

--- sncopy.py
+++ sncopy.py
@@ -5,23 +5,28 @@
 Descripción: Solo hay una función, vea su documentación.
 """
 
-from shlex import quote
-from os import system as ossystem
 from sys import argv as sysargv
 from subprocess import Popen, PIPE
+from re import search as research, compile as recompile
 
 # VARs
 DA = dict(md5sum="d41d8cd98f00b204e9800998ecf8427e", sha256sum="e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855", sha512sum="cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e")
 
+# PATRONES
+pat0 = recompile(r"^.*?:\ directory$|^.*?\:\ cannot\ open\ `.*?'\ \(No\ such\ file\ or\ directory\)$")
+pat1 = recompile(r"^.*?:\ directory$")
+
 def sncopy(ORIGEN, DESTINO, ALGO="md5sum", /):
     """Esta función copia y sobreescribe un archivo SOLO en caso de que sean distintos, esto se comprueba usando comparando sumas de comprobación md5."""
-    ORIGEN = quote(ORIGEN)
-    DESTINO = quote(DESTINO)
-    if ossystem(f'file {ORIGEN} | grep "^.*: directory$\\|^.*: cannot open \\`.*\' (No such file or directory)" > /dev/null') == 0:
+
+    checkI0 = Popen(['file', ORIGEN], stdout=PIPE, stderr=PIPE).communicate()[0].decode("utf-8")[:-1]
+    checkI1 = Popen(["file", DESTINO], stdout=PIPE, stderr=PIPE).communicate()[0].decode("utf-8")[:-1]
+
+    if research(pat0, checkI0):
         print('El origen no existe o es un directorio')
         return
 
-    elif ossystem(f'file {DESTINO} | grep "^.*: directory$" > /dev/null') == 0:
+    elif research(pat1, checkI1):
         print('No se puede operar sobre directorios')
         return
 
@@ -40,7 +45,7 @@ def sncopy(ORIGEN, DESTINO, ALGO="md5sum", /):
     # LOGICA
     if ORESULT != DRESULT or (ORESULT == DA[ALGO] and DRESULT == DA[ALGO]):
         print(f'Realizando la copia')
-        ossystem(f'cp {ORIGEN} {DESTINO}')
+        Popen(['cp', ORIGEN, DESTINO])
 
 if __name__ == "__main__":
     try:
